96b7bee62b42869f2bed1a060e4eb273b96967c5
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-06-10 11:40:22 +0200

    Changed the Documents class, added age, location and gender fields

--- twitter/models/mongo_models.py
+++ twitter/models/mongo_models.py
@@ -12,82 +12,90 @@ from datetime import datetime
 from bson.objectid import ObjectId
 import pymongo
 
-def connectDB(dbname = 'TwitterDB'):
-	connect(dbname, host="127.0.0.1", port=27017, read_preference= True)
+
+def connectDB(dbname='TwitterDB'):
+    connect(dbname, host="127.0.0.1", port=27017, read_preference=True)
+
 
 class Word(EmbeddedDocument):
-	_auto_id_field = False
-	word = StringField(max_length = 255)
-	pos = ListField()
-	count = FloatField()
-	tf = FloatField()
-	idf = FloatField()
+    _auto_id_field = False
+    word = StringField(max_length=255)
+    pos = ListField()
+    count = FloatField()
+    tf = FloatField()
+    idf = FloatField()
 
-	meta = {
-		'ordering': ['+word']
-	}
+    meta = {
+        'ordering': ['+word']
+    }
+
 
 class Documents(Document):
-	_auto_id_field = False
-	tweetID = StringField(max_length = 255, required = True, unique=True)
-	rawText = StringField()
-	cleanText = StringField() #intermediate text without stopwords, punctuation, etc
-	lemmaText = StringField() #lemma text
-	#the date the element was inserted in the database
-	createdAt = DateTimeField(default=datetime.now)
-	gender = StringField()
-	hashtags = ListField()
-	attags = ListField()
-	date = DateTimeField()
-	language = StringField()
-	author = StringField()
-	tags = ListField()
-	words = ListField(EmbeddedDocumentField("Word"))
-	
+    _auto_id_field = False
+    # tweetID = StringField(max_length=255, required=True, unique=True) this is the _id field
+    rawText = StringField()
+    cleanText = StringField()  # intermediate text without stopwords, punctuation, etc
+    lemmaText = StringField()  # lemma text
+    # the date the element was inserted in the database
+    createdAt = DateTimeField(default=datetime.now)
+    gender = StringField()
+    hashtags = ListField()
+    attags = ListField()
+    date = DateTimeField()
+    language = StringField()
+    words = ListField(EmbeddedDocumentField("Word"))
+    tags = ListField()
+    geoLocation = ListField()  # geo location, list with 2 elements: [x, y]
+    author = StringField() # author ID
+    gender = StringField()  # male or female
+    age = IntField()  # age of author
 
-	meta = {
-		'ordering': ['+createdAt']
-	}
+    meta = {
+        'ordering': ['+createdAt']
+    }
 
 
 class InvertedIndex(Document):
-	word = StringField(max_length = 255, required = True, unique=True)
-	docIDs = ListField()
-	createdAt = DateTimeField(default=datetime.now) 
+    word = StringField(max_length=255, required=True, unique=True)
+    docIDs = ListField()
+    createdAt = DateTimeField(default=datetime.now)
 
-	meta = { 
-			'ordering': ['+createdAt']
-	}
+    meta = {
+        'ordering': ['+createdAt']
+    }
+
 
 class Docs(EmbeddedDocument):
-	_auto_id_field = False
-	docID = ObjectIdField()
-	count = FloatField()
-	tf = FloatField()
-	wtype = StringField(max_length = 255)
+    _auto_id_field = False
+    docID = ObjectIdField()
+    count = FloatField()
+    tf = FloatField()
+    wtype = StringField(max_length=255)
+
 
 class Vocabulary(Document):
-	word = StringField(max_length = 255, required = True, unique=True)
-	idf = FloatField()
-	#wtype = StringField(max_length = 255, required = True) #not yet implemented
-	createdAt = DateTimeField(default=datetime.now)
-	docIDs = ListField(EmbeddedDocumentField("Docs"))
+    word = StringField(max_length=255, required=True, unique=True)
+    idf = FloatField()
+    # wtype = StringField(max_length = 255, required = True) #not yet implemented
+    createdAt = DateTimeField(default=datetime.now)
+    docIDs = ListField(EmbeddedDocumentField("Docs"))
 
-	meta = {
-			'indexes': [
-				{
-					'fields': ['+word'],
-					'unique': True,
-					'sparse': False
-				}
-		]
-	}
+    meta = {
+        'indexes': [
+            {
+                'fields': ['+word'],
+                'unique': True,
+                'sparse': False
+            }
+        ]
+    }
+
 
 class NamedEntities(Document):
-	docID = ObjectIdField()
-	createdAt = DateTimeField(default=datetime.now)
-	gpe = ListField()
-	person = ListField()
-	organization = ListField()
-	facility = ListField()
-	location = ListField()
+    docID = ObjectIdField()
+    createdAt = DateTimeField(default=datetime.now)
+    gpe = ListField()
+    person = ListField()
+    organization = ListField()
+    facility = ListField()
+    location = ListField()
