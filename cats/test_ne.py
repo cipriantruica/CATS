56426c4f11a2137e2169e4eb494162d0ee457645
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-06-15 14:30:24 +0200

    Test NER

--- cats/test_ne.py
+++ cats/test_ne.py
@@ -14,11 +14,14 @@ if __name__ == '__main__':
     query_and = {"$and": [{ "words.word": "shit"}, {'words.word': "fuck" } ], "date": {"$gt": "2015-04-10", "$lte":  "2015-04-12"}, 'namedEntities': {'$exists': 'true'}}
     query = {'namedEntities': {'$exists': 'true'}}
 
+    start = time.time()
     ne = NEIndex(dbname)
     ne.createIndex(query)
+    end = time.time()
+    print 'NE creation time:', (end-start)
 
-    cursor = db.named_entities.find()
     start = time.time()
+    cursor = db.named_entities.find(sort=[('count',pymongo.DESCENDING)])
     idx = 0
     for elem in cursor:
         print elem['entity'], elem['count'], elem['type']
