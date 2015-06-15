6c15724933ae6847a9544d614dde9dcb0bd5865d
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-06-15 12:22:17 +0200

    Test NER

--- cats/test_ne.py
+++ cats/test_ne.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     client = pymongo.MongoClient()
     dbname = 'TwitterDB'
     db = client[dbname]
-    #TO DO see how to use exists!!!!
+    # TO DO see how to use exists!!!!
     # query_or = {"words.word" : {"$in": ["shit", "fuck"] }, "date": {"$gt": "2015-04-10", "$lte":  "2015-04-12"}, 'namedEntities': {'$exists': 'true'}}
     query_or = {'namedEntities': {'$exists': 'true'}}
     cursor = db.documents.find(spec=query_or, fields={'namedEntities': 1, '_id': 0})
@@ -39,23 +39,25 @@ if __name__ == '__main__':
     # cursor.rewind()
 
 
-    print 'test2'
-    namedEntitiesDict2 = list()
+    # print 'test2'
+    # namedEntitiesDict2 = list()
+    # for elem in cursor:
+    #     for ne in elem['namedEntities']:
+    #         if ne['type'] != 'GPE':
+    #             ok = True
+    #             for elem in namedEntitiesDict2:
+    #                 if elem['entity'] == ne['entity']:
+    #                     ok = False
+    #                     elem['count'] += 1
+    #                     break
+    #             if ok:
+    #                 namedEntitiesDict2.append({'entity': ne['entity'], 'count': 1, 'type': ne['type']})
+    #
+    #
+    #
+    # for elem in namedEntitiesDict2:
+    #     print elem['entity'], elem['count'], elem['type']
+
     for elem in cursor:
-        for ne in elem['namedEntities']:
-            if ne['type'] != 'GPE':
-                ok = True
-                for elem in namedEntitiesDict2:
-                    if elem['entity'] == ne['entity']:
-                        ok = False
-                        elem['count'] += 1
-                        break
-                if ok:
-                    namedEntitiesDict2.append({'entity': ne['entity'], 'count': 1, 'type': ne['type']})
-
-
-
-    for elem in namedEntitiesDict2:
-        print elem['entity'], elem['count'], elem['type']
-
+        print elem
 
