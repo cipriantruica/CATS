81044f9d3eb7999b1f87fca3ac5a43c404677861
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-06-23 11:37:49 +0200

    LDA test3

--- cats/test_lda3.py
+++ cats/test_lda3.py
@@ -24,44 +24,44 @@ if __name__ == '__main__':
     documents = []
     documentsDB = db.documents.find({'gender': 'homme'}, {'lemmaText': 1, '_id': 0})
     for document in documentsDB:
-        print document['lemmaText'].encode('utf8')
-    #     documents.append(document['lemmaText'].split())
-    # dictionary = corpora.Dictionary(documents)
-    # corpus = [dictionary.doc2bow(document) for document in documents]
+        # print document['lemmaText'].encode('utf8')
+        documents.append(document['lemmaText'].split())
+    dictionary = corpora.Dictionary(documents)
+    corpus = [dictionary.doc2bow(document) for document in documents]
+
+    topic_model = TopicModeling(id2word=dictionary, corpus=corpus)
+    print 'LDA using lemma text'
+    for topic in topic_model.topicsLDA(num_topics=15):
+        print topic, '\n'
+    end = time.time()
+
+    print 'LDA TF time:', (end - start)
+
+    # just test for the corpus
+    # to see where things go wrong when constructing the Matrix Market
+    # od = collections.OrderedDict(sorted(dictionary.items()))
     #
-    # topic_model = TopicModeling(id2word=dictionary, corpus=corpus)
-    # print 'LDA using lemma text'
-    # for topic in topic_model.topicsLDA(num_topics=15):
-    #     print topic, '\n'
-    # end = time.time()
-    #
-    # print 'LDA TF time:', (end - start)
-    #
-    # # just test for the corpus
-    # # to see where things go wrong when constructing the Matrix Market
-    # # od = collections.OrderedDict(sorted(dictionary.items()))
-    # #
-    # # for key in od:
-    # #     print key, od[key]
-    # # print corpus
-    #
-    # start = time.time()
-    # vi = VocabularyIndex(dbname='TwitterDB')
-    # vi.createIndex(query={'gender': 'femme'})
-    #
-    # # entire vocabulary
-    # mm = MarketMatrix(dbname='TwitterDB')
-    # mm.build(query=True)
-    #
-    # # without creating the market matrix file
-    # id2word, id2tweetID, corpus = mm.buildTFMM()
-    #
-    # # for elem in id2word:
-    # #     print elem, id2word[elem]
-    #
-    # print 'LDA with Matrix Market'
-    # start = time.time()
-    # for topic in topic_model.topicsLDA(num_topics=15):
-    #     print topic, '\n'
-    # end = time.time()
-    # print 'LDA TF time:', (end - start)
+    # for key in od:
+    #     print key, od[key]
+    # print corpus
+
+    start = time.time()
+    vi = VocabularyIndex(dbname='TwitterDB')
+    vi.createIndex(query={'gender': 'femme'})
+
+    # entire vocabulary
+    mm = MarketMatrix(dbname='TwitterDB')
+    mm.build(query=True)
+
+    # without creating the market matrix file
+    id2word, id2tweetID, corpus = mm.buildTFMM()
+
+    # for elem in id2word:
+    #     print elem, id2word[elem]
+
+    print 'LDA with Matrix Market'
+    start = time.time()
+    for topic in topic_model.topicsLDA(num_topics=15):
+        print topic, '\n'
+    end = time.time()
+    print 'LDA TF time:', (end - start)
