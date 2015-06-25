d125fdd0c9d23828a4d1d2586c7a227968f309c8
Ciprian Truică <ciprian.octavian.truica@gmail.com>
2015-06-25 13:36:39 +0200

    MABED files

--- cats/mabed/mabed_files.py
+++ cats/mabed/mabed_files.py
@@ -19,10 +19,14 @@ class MabedFiles:
         self.query = {}
 
     # slice is given in seconds
-    def buildFiles(self, query={}, slice=3600, filepath=''):
+    def buildFiles(self, query={}, slice=3600, filepath='./'):
         try:
+            #add / at the end of the file path if it doesn't exist
+            if filepath[:-1] != '/':
+                filepath += '/'
+            # copy query into the class query not to change the original query
+            self.query = query.copy()
             # get date
-            self.query = query
             if self.query and self.query.get("date"):
                 startDate = datetime.strptime(self.query["date"]["$gt"] + " 00:00:00", '%Y-%m-%d %H:%M:%S')
                 endDate = datetime.strptime(self.query["date"]["$lte"] + " 00:00:00", '%Y-%m-%d %H:%M:%S')
@@ -55,6 +59,8 @@ class MabedFiles:
             print e
 
 if __name__ == '__main__':
+    # make folders if you want to use the test
+    # look at the filepath parameter used for this
     mf = MabedFiles(dbname='TwitterDB')
     query1 = {'words.word': {'$in': [u'fuck']}}
     query2 = {'date': {'$lte': u'2015-04-10', '$gt': u'2015-04-08'}}
@@ -62,23 +68,23 @@ if __name__ == '__main__':
     query4 = {'date': {'$lte': u'2015-04-10', '$gt': u'2015-04-08'}, 'gender': u'femme', 'age': {'$in': [u'5-11', u'12-18', u'45-60', u'61-99']}, 'words.word': {'$in': [u'fuck']}}
     query5 = {}
     start = time()
-    mf.buildFiles(query1, filepath='1/', slice=3600)
+    mf.buildFiles(query1, slice=3600)
     end = time()
     print 'Time',(end-start), query1
     start = time()
-    mf.buildFiles(query2, filepath='2/', slice=3600)
+    mf.buildFiles(query2, filepath='test/2', slice=3600)
     end = time()
     print 'Time',(end-start), query2
     start = time()
-    mf.buildFiles(query3, filepath='3/', slice=3600)
+    mf.buildFiles(query3, filepath='test/3/', slice=3600)
     end = time()
     print 'Time',(end-start), query3
     start = time()
-    mf.buildFiles(query4, filepath='4/', slice=3600)
+    mf.buildFiles(query4, filepath='test/4', slice=3600)
     end = time()
     print 'Time',(end-start), query4
     start = time()
-    mf.buildFiles(query5, filepath='5/', slice=3600)
+    mf.buildFiles(query5, filepath='test/5/', slice=3600)
     end = time()
     print 'Time',(end-start), query5
 
