352d39325c99d698cbef663cd67d1d0bc5915c85
Adrien Guille <adrien@mediamining.univ-lyon2.fr>
2015-07-27 14:05:21 +0200

    Fix shell script for importing data

--- cats/run_new.sh
+++ cats/run_new.sh
@@ -8,15 +8,17 @@
 #__status__ = "Production"
 
 #args:
-#1 - corpus csv format
-#2 - delimiter (t -tab, c - comma, s - Semicolon)
-#3 - has header (0 - false, 1 - true)
-#4 - Database name
-#5 - language, currently supports EN (English) and FR (French) - only lemmatization, not NER
-#6 - type of opperations to do: 0(insert), 1(update), 2(delete)
-#7 - mode for lemmatizer (works only for English): 0 (fast but not accurate), 1 (slow but accurate)
-#8 - 1 - use serialized version, 0 - use parallelized version
-#example: python populateDB.py DATA_SETS/news_articles/rss.csv t 1 2 ERICDB EN 1 1
+#  1 - corpus csv format
+#  2 - delimiter (t - tab, c - comma, s - Semicolon)
+#  3 - has header (0 - false, 1 - true)
+#  4 - Database name
+#  5 - host = host of the database
+#  6 - port of the database - integer
+#  7 - language, currently supports EN (English) and FR (French) - only lemmatization, not NER
+#  8 - type of opperations to do: 0(insert), 1(update), 2(delete)
+#  9 - mode for lemmatizer (works only for English): 0 (fast but not accurate), 1 (slow but accurate)
+# 10 - integer: 0 - use parallelized version, 1 - use serialized version
+#example: python populateDB.py DATA_SETS/news_articles/rss.csv t 1 2 ERICDB localhost 27017 EN 1 1 0
 
 HEADER=0
 DELIMITER="t"
@@ -28,9 +30,11 @@ SERIAL=0
 #on server
 #FILE="/home/cats/data/CATS_demo_May-12-2015_3.csv"
 #on Ciprian computer
-FILE="../DATA_SETS/tweets/CATS_demo_200.csv"
-DB="TwitterDB_at"
+FILE="../DATA_SETS/tweets/CATS_HFB.csv"
+DB="TwitterDB_demo"
+HOST="localhost"
+PORT=27017
 echo $FILE
-python testing_tweets_new.py $FILE $DELIMITER $HEADER $DB $LANGUAGE $INIT $MODE $SERIAL
+python testing_tweets_new.py $FILE $DELIMITER $HEADER $DB $HOST $PORT $LANGUAGE $INIT $MODE $SERIAL
 
 
